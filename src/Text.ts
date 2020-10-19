29859ddf81981ed042c58a063c36556706531117
Paul Grebenc <paul@grebenc.ca>
2020-10-18 22:36:25 -0400

    Convert text interlude to type script (#6)
    * Convert TextInterlude to TypeScript.
    
    * Convert TextInterlude tests to TypeScript.

--- src/Text.ts
+++ src/Text.ts
@@ -44,48 +44,8 @@ export class Text {
     get active(): boolean {
         return this._active;
     }
-}
 
-// const { v4: uuidv4 } = require('uuid');
-//
-// var Text = function(content, font, color, startX, startY) {
-//     debug('Text constructor');
-//     this._id = uuidv4();
-//     this._content = content;
-//     this._font = font;
-//     this._color = color;
-//     this._x = startX;
-//     this._y = startY;
-//     this._active = true;
-// };
-//
-// Text.prototype.getId = function() {
-//     return this._id;
-// };
-//
-// Text.prototype.getCoordinates = function() {
-//     return {x: this._x, y: this._y};
-// };
-//
-// Text.prototype.tick = function() {
-//     debug('Text.tick');
-// };
-//
-// Text.prototype.getContent = function() {
-//     return this._content;
-// };
-//
-// Text.prototype.getColor = function() {
-//     return this._color;
-// };
-//
-// Text.prototype.getFont = function() {
-//     return this._font;
-// };
-//
-// Text.prototype.isActive = function() {
-//     debug('Text.isActive: ' + this._active);
-//     return this._active
-// };
-//
-// module.exports = Text;
+    set active(value: boolean) {
+        this._active = value;
+    }
+}
