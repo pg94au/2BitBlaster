f96b1c7c7f9c6613dbed0899d23bf239ab33a836
Paul Grebenc <paul@grebenc.ca>
2020-11-05 22:27:34 -0500

    Extracted DirectionState from Joystick.

--- src/devices/Joystick.ts
+++ src/devices/Joystick.ts
@@ -1,40 +1,10 @@
 import Debug from "debug";
-import {Direction} from './Direction';
-
 const debug = Debug("Blaster:Joystick");
 
-class DirectionState {
-    private static _eventCounter: number = 0;
-    private readonly _direction: Direction;
-    private _isActive: boolean = false;
-    private _eventNumber: number = 0;
-
-    constructor(direction: Direction) {
-        this._direction = direction;
-    }
-
-    get direction(): Direction {
-        return this._direction;
-    }
-
-    get isActive(): boolean {
-        return this._isActive;
-    }
-
-    get eventNumber(): number {
-        return this._eventNumber;
-    }
-
-    set active(state: boolean) {
-        if (this._isActive !== state) {
-            this._isActive = state;
-            this._eventNumber = ++DirectionState._eventCounter;
-        }
-    }
-}
+import {Direction} from './Direction';
+import {DirectionState} from "./DirectionState";
 
 export class Joystick {
-    private _eventCounter: number = 0;
     private _fireState: boolean = false;
     private readonly _up = new DirectionState(Direction.Up);
     private readonly _down = new DirectionState(Direction.Down);
