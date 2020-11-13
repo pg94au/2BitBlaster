cb1b39801ce8aa60b07e222888243fbca901b2ba
Paul Grebenc <paul@grebenc.ca>
2020-11-13 18:05:22 -0500

    Convert renderer to type script (#49)
    * Rendering sprites works with new pixi.js.  Text not rendering yet.
    
    * Removing commented out code.
    
    * Separated SpriteDetail class to new file.
    
    * Fix rendering of text in the game.

--- src/Main.ts
+++ src/Main.ts
@@ -5,29 +5,18 @@ import AudioPlayer from './devices/AudioPlayer';
 import {Clock} from './timing/Clock';
 import {Game} from './Game';
 import {Joystick} from './devices/Joystick';
-import Renderer from './devices/Renderer';
+import {Renderer} from './devices/Renderer';
 
 Debug.disable();
 
-console.log('Hello world');
-
 export default class Main {
-    // @ts-ignore
-    public AudioPlayer: AudioPlayer = AudioPlayer;
-    // @ts-ignore
-    public Clock: Clock = Clock;
-    // @ts-ignore
-    public Game: Game = Game;
-    // @ts-ignore
-    public Joystick: Joystick = Joystick;
-    // @ts-ignore
-    public Renderer: Renderer = Renderer;
+    public AudioPlayer: typeof AudioPlayer = AudioPlayer;
+    public Clock: typeof Clock = Clock;
+    public Game: typeof Game = Game;
+    public Joystick: typeof Joystick = Joystick;
+    public Renderer: typeof Renderer = Renderer;
 
     constructor() {
         debug("Constructor called.")
-    }
-
-    sayHello(): void {
-        console.log('Hi there');
     }
 }
