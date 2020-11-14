df5eb1db788aa1a266c4064a31ad699a6c4e2213
Paul Grebenc <paul@grebenc.ca>
2020-11-13 20:08:05 -0500

    Fix tests.  Simplify AudioPlayer.

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,7 @@
 import Debug from "debug";
 const debug = Debug("Blaster:Main");
 
-import {AudioPlayer} from './devices/AudioPlayer';
+import {AudioContextAudioPlayer} from "./devices/AudioContextAudioPlayer";
 import {Clock} from './timing/Clock';
 import {Game} from './Game';
 import {Joystick} from './devices/Joystick';
@@ -10,7 +10,7 @@ import {PixiRenderer} from "./devices/PixiRenderer";
 Debug.disable();
 
 export default class Main {
-    public AudioPlayer: typeof AudioPlayer = AudioPlayer;
+    public AudioPlayer: typeof AudioContextAudioPlayer = AudioContextAudioPlayer;
     public Clock: typeof Clock = Clock;
     public Game: typeof Game = Game;
     public Joystick: typeof Joystick = Joystick;
