5626a23e1ee30a5028086e35acdd381a050a2761
Paul Grebenc <paul@grebenc.ca>
2020-11-13 23:07:18 -0500

    Updated resource loader.

--- src/devices/AudioContextAudioPlayer.ts
+++ src/devices/AudioContextAudioPlayer.ts
@@ -1,6 +1,7 @@
 import Debug from "debug";
 const debug = Debug("Blaster:AudioPlayer");
-const ResourceLoader = require('resource-loader');
+import {Loader} from 'resource-loader';
+import ResourceMap = Loader.ResourceMap;
 
 import {AudioPlayer} from "./AudioPlayer";
 
@@ -11,56 +12,40 @@ export class AudioContextAudioPlayer implements AudioPlayer {
     constructor() {
         debug('AudioPlayer constructor');
 
-        ResourceLoader.Resource.setExtensionLoadType('mp3', ResourceLoader.Resource.LOAD_TYPE.XHR);
-        ResourceLoader.Resource.setExtensionXhrType('mp3', ResourceLoader.Resource.XHR_RESPONSE_TYPE.BUFFER);
-
         // @ts-ignore
         let AudioContext = window.AudioContext || window.webkitAudioContext;
         this._audioContext = new AudioContext();
     }
 
     preLoadSounds(sounds: any[], onSuccess: () => void, onFailure: (errors: any) => void) {
-        let resourceLoader = new ResourceLoader();
+        let loader = new Loader();
         let errors: any[] = [];
 
         let errorHandler = (error: any) => {
             debug('Error event while loading audio resources: ' + error);
             errors.push(error);
         };
-        resourceLoader.on('error', errorHandler);
+        loader.onError.add(errorHandler);
 
         for (let sound of sounds) {
-            resourceLoader.add(sound.name, sound.url);
+            loader.add(sound.name, sound.url);
         }
 
-        resourceLoader.load((rl: any, resources: any) => {
+        loader.load((rl: Loader, resourceMap: ResourceMap) => {
             if (errors.length > 0) {
                 onFailure(errors);
             }
             else {
-                debug('Successfully loaded ' + resources.length + ' sounds.');
-                // self.resources = resources;
+                debug('Successfully loaded ' + resourceMap.length + ' sounds.');
 
-                let decodePromises = [];
-                let decodeNames: any[] = [];
-                for (let i in resources) {
-                    decodePromises.push(this._audioContext.decodeAudioData(resources[i].data));
-                    decodeNames.push(resources[i].name);
-                }
-                Promise.all(decodePromises).then(
-                    (decodedAudio: any) => {
-                        debug('Decoded ' + decodedAudio.length + ' audio samples.');
-                        for (let i = 0; i < decodedAudio.length; i++) {
-                            this._decodedAudio[decodeNames[i]] = decodedAudio[i];
-                        }
-
-                        setTimeout(onSuccess, 0);
-                    },
-                    (reason: any) => {
-                        debug('Promise.all rejected: ' + reason);
+                for (let name in resourceMap) {
+                    if (resourceMap.hasOwnProperty(name)) {
+                        this._decodedAudio[name] = resourceMap[name]!.data;
                     }
-                );
-            };
+                }
+
+                onSuccess();
+            }
         });
     }
 
@@ -69,10 +54,8 @@ export class AudioContextAudioPlayer implements AudioPlayer {
         if (decodedAudio) {
             debug('Playing sound ' + soundName);
 
-            let source = this._audioContext.createBufferSource();
-            source.buffer = this._decodedAudio[soundName];
-            source.connect(this._audioContext.destination);
-            source.start(0);
+            let audio = decodedAudio.cloneNode(true);
+            audio.play();
         }
         else {
             throw new Error('Cannot find sound ' + soundName);
