86fe3784a6e384f2289f4f81f477c6154d45de5f
Paul Grebenc <paul@grebenc.ca>
2021-10-17 19:49:44 -0400

    88 (#90)
    * Issue #88 - Reference certificate by name in terraform instead of ARN.
    
    * Issue #88: Certificates for CloudFront must always be in us-east-1.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region  = var.region
 }
 
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
 terraform {
   backend "s3" {
     bucket = "2bitblaster-tfstate"
