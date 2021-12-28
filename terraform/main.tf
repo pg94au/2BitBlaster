0bd6df0000bc43c8849c0ca0dce0953bce360738
Paul Grebenc <paul@grebenc.ca>
2021-12-28 17:01:44 -0500

    Add missing attribtutes for blocking public access.

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,4 +74,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
