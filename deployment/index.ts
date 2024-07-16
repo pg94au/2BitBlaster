ce4474b309097486b6fedca293364621c7a55c95
Paul Grebenc <pgrebenc@opentext.com>
2024-07-16 15:02:49 -0400

    Pulumi preview seems happy with this.

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 import * as docker from "@pulumi/docker";
 
 
@@ -8,13 +9,53 @@ const callerIdentity = aws.getCallerIdentity({});
 const accountId = callerIdentity.then(identity => identity.accountId);
 
 
-// Define the Docker image
-const highScoreLambdaImage = new docker.Image("highScoreLambdaImage", {
-    build: {
-        context: "../highScores",
-    },
-    imageName: "2-bit-blaster-high-score",
+// Create a docker image repository
+const ecrRepository = new awsx.ecr.Repository(
+    "2-bit-blaster-ecr-repository",
+    { name: "2-bit-blaster-ecr-repository" }
+);
+
+const highScoreLambdaImage = new awsx.ecr.Image("2-bit-blaster-lambda-image", {
+    repositoryUrl: ecrRepository.url,
+    context: "../highScores/",
+    dockerfile: "Dockerfile"
 });
+
+
+// const authToken = aws.ecr.getAuthorizationTokenOutput({
+//     registryId: ecrRepository.repository.registryId,
+// });
+
+// const highScoreLambdaImage = new docker.Image("2-bit-blaster-lambda-image", {
+//     build: {
+//         args: {
+//             BUILDKIT_INLINE_CACHE: "1",
+//         },
+//         cacheFrom: {
+//             images: [pulumi.interpolate`${ecrRepository.repository.repositoryUrl}:latest`],
+//         },
+//         context: "../highScores/",
+//         dockerfile: "Dockerfile",
+//     },
+//     imageName: pulumi.interpolate`${ecrRepository.repository.repositoryUrl}:latest`,
+//     registry: {
+//         password: pulumi.secret(authToken.apply(authToken => authToken.password)),
+//         server: ecrRepository.repository.repositoryUrl,
+//     },
+// });
+
+
+// const highScoreLambdaImage = ecrRepository.buildAndPushImage({
+//     context: ".../highScores",
+// });
+
+// // Define the Docker image
+// const highScoreLambdaImage = new docker.Image("highScoreLambdaImage", {
+//     build: {
+//         context: "../highScores",
+//     },
+//     imageName: "2-bit-blaster-high-score",
+// });
 
 
 // // Create an IAM role for the Lambda function
@@ -33,12 +74,13 @@ const highScoreLambdaImage = new docker.Image("highScoreLambdaImage", {
 
 
 // Create role for highScore lambda
-const highScoreLambdaRole = new aws.iam.Role("highScoreLambdaRole", {
+const highScoreLambdaRole = new aws.iam.Role("2-bit-blaster-high-score-lambda-role", {
+    name: "2-bit-blaster-high-score-lambda-role",
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
 });
 
 // Attach the AWSLambdaBasicExecutionRole policy to the role
-new aws.iam.RolePolicyAttachment("highScoreLambdaBasicExecutionRolePolicyAttachment", {
+new aws.iam.RolePolicyAttachment("2-bit-blaster-high-score-lambda-basic-execution-role-policy-attachment", {
     role: highScoreLambdaRole,
     policyArn: aws.iam.ManagedPolicies.AWSLambdaBasicExecutionRole,
 });
@@ -58,21 +100,22 @@ const highScoreSsmPolicyDocument = aws.iam.getPolicyDocument({
         resources: [`arn:aws:ssm:ca-central-1:${accountId}:parameter/2BitBlaster/highScore`],
     }],
 });
-const highScoreLambdaSsmPolicy = new aws.iam.Policy("highScoreLambdSsmPolicy", {
-    name: "highScore-lambda-ssm-policy",
+const highScoreLambdaSsmPolicy = new aws.iam.Policy("2-bit-blaster-high-scorelambda-ssm-policy", {
+    name: "2-bit-blaster-high-scorelambda-ssm-policy",
     description: "Policy to allow 2-Bit Blaster high score lambda to access parameter store",
     policy: highScoreSsmPolicyDocument.then(policyDocument => policyDocument.json),
 });
-const highScoreLambdaSsmRolePolicyAttachment = new aws.iam.RolePolicyAttachment("highScoreLambdaSsmRolePolicyAttachment", {
+const highScoreLambdaSsmRolePolicyAttachment = new aws.iam.RolePolicyAttachment("2-bit-blaster-high-scorelambda-ssm-role-policy-attachment", {
     role: highScoreLambdaRole.name,
     policyArn: highScoreLambdaSsmPolicy.arn,
 });
 
 
 // Create the Lambda function
-const lambda = new aws.lambda.Function("highScoreLambdaFunction", {
+const lambda = new aws.lambda.Function("2-bit-blaster-high-score-lambda-function", {
+    name: "2-bit-blaster-high-score-lambda-function",
     packageType: "Image",
-    imageUri: highScoreLambdaImage.imageName,
+    imageUri: highScoreLambdaImage.imageUri,
     role: highScoreLambdaRole.arn,
 });
 
