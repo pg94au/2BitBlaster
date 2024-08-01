f3ceaf498128f7a2a4526d422d0f236a2c3616f4
Paul Grebenc <paul@grebenc.ca>
2024-08-01 13:36:22 -0400

    Pulumi now deploys a working high score endpoint.

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,3 +1,4 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
@@ -31,7 +32,7 @@ const highScoreSsmPolicyDocument = aws.iam.getPolicyDocument({
     statements: [{
         effect: "Allow",
         actions: ["ssm:GetParameter", "ssm:PutParameter"],
-        resources: [`arn:aws:ssm:ca-central-1::parameter/2BitBlaster/highScore`],
+        resources: ["arn:aws:ssm:ca-central-1:663866322745:parameter/2BitBlaster/HighScore"]
     }],
 });
 const highScoreLambdaSsmPolicy = new aws.iam.Policy("2-bit-blaster-high-score-lambda-ssm-policy", {
@@ -45,9 +46,56 @@ const highScoreLambdaSsmRolePolicyAttachment = new aws.iam.RolePolicyAttachment(
 });
 
 // Create the Lambda function
-const lambda = new aws.lambda.Function("2-bit-blaster-high-score-lambda-function", {
+const highScoreLambda = new aws.lambda.Function("2-bit-blaster-high-score-lambda-function", {
     name: "2-bit-blaster-high-score-lambda-function",
     packageType: "Image",
     imageUri: highScoreLambdaImage.imageUri,
     role: highScoreLambdaRole.arn,
 });
+
+// Create an API Gateway REST API
+const api = new aws.apigateway.RestApi("2-bit-blaster-api-gateway", {
+    description: "API Gateway for 2-Bit Blaster",
+});
+
+// Create a resource for the high score endpoint
+const highScoreApiGatewayResource = new aws.apigateway.Resource("2-bit-blaster-api-gateway-high-score-resource", {
+    restApi: api.id,
+    parentId: api.rootResourceId,
+    pathPart: "highScore",
+});
+
+// Create a method for the resource
+const highScoreApiGatewayMethod = new aws.apigateway.Method("2-bit-blaster-api-gateway-high-score-method", {
+    restApi: api.id,
+    resourceId: highScoreApiGatewayResource.id,
+    httpMethod: "POST",
+    authorization: "NONE",
+});
+
+// Integrate the method with the Lambda function
+const integration = new aws.apigateway.Integration("2-bit-blaster-api-gateway-high-score-integration", {
+    restApi: api.id,
+    resourceId: highScoreApiGatewayResource.id,
+    httpMethod: highScoreApiGatewayMethod.httpMethod,
+    integrationHttpMethod: "POST",
+    type: "AWS_PROXY",
+    uri: highScoreLambda.invokeArn,
+});
+
+// Grant API Gateway permission to invoke the high score lambda function
+const highScoreLambdaApiPermission = new aws.lambda.Permission("2-bit-blaster-api-gateway-invoke-high-score-permission", {
+    action: "lambda:InvokeFunction",
+    function: highScoreLambda.arn,
+    principal: "apigateway.amazonaws.com",
+    sourceArn: pulumi.interpolate`${api.executionArn}/*/*`,
+});
+
+// Deploy the REST API
+const deployment = new aws.apigateway.Deployment("2-bit-blaster-api-gateway-deployment", {
+    restApi: api.id,
+    stageName: "prod",
+}, { dependsOn: [integration] });
+
+// Export the URL of the API
+export const apiUrl = deployment.invokeUrl;
