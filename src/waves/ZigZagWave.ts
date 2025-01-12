ad86087d9022638f09806e7e8ddd1e2ae2c66cb0
Paul Grebenc <paul@grebenc.ca>
2025-01-11 21:40:27 -0500

    Use scheduler in deploying zaggers.

--- src/waves/ZigZagWave.ts
+++ src/waves/ZigZagWave.ts
@@ -5,9 +5,10 @@ import {random} from 'underscore';
 import {AudioPlayer} from "../devices/AudioPlayer";
 import {Clock} from "../timing/Clock";
 import {Point} from '../Point';
-import {Zagger} from '../enemies/Zagger';
+import {Scheduler} from '../timing/Scheduler';
 import {Wave} from './Wave';
 import {World} from "../World";
+import {Zagger} from '../enemies/Zagger';
 
 export class ZigZagWave implements Wave {
     private readonly _audioPlayer: AudioPlayer;
@@ -16,11 +17,21 @@ export class ZigZagWave implements Wave {
     private _addNextEnemyAt: Date = new Date();
     private _numberOfEnemiesLeftToDeploy: number = 15;
 
+    private readonly _scheduler: Scheduler;
+
     constructor(audioPlayer: AudioPlayer, world: World, clock: Clock) {
         debug('ZigZagWave constructor');
         this._audioPlayer = audioPlayer;
         this._world = world;
         this._clock = clock;
+
+        this._scheduler = new Scheduler(clock);
+
+        this._scheduler.scheduleOperation(
+            'deploy',
+            0,
+            () => { this.deployZagger() }
+        );
     }
 
     get isActive(): boolean {
@@ -32,26 +43,29 @@ export class ZigZagWave implements Wave {
     tick(): void {
         debug('ZigZagWave.tick');
 
-        //TODO: This is just a simple demonstration of how a wave can manage adding enemies to the world.
+        this._scheduler.executeDueOperations();
+    }
+
+    deployZagger(): void {
         if (this._numberOfEnemiesLeftToDeploy > 0) {
-            // Add new enemy when the time comes, but only if a maximum allowed aren't already active.
-            if ((this._addNextEnemyAt <= new Date()) && (this._world.activeEnemies.length < 15)) {
-                // Space out the addition of enemies.
-                this._addNextEnemyAt = new Date(Date.now() + 250);
+            const worldDimensions = this._world.dimensions;
+            const zaggerStartingPoint = new Point(
+                Math.floor(random(0, worldDimensions.width-50)),
+                -20
+            );
+            const zaggerHomeX = (5-(this._numberOfEnemiesLeftToDeploy-1)%5) * 80;
+            const zaggerHomeY = 100 + Math.floor((this._numberOfEnemiesLeftToDeploy-1)/5) * 70;
+            const zaggerHomePosition = new Point(zaggerHomeX, zaggerHomeY);
+            const zagger = new Zagger(this._audioPlayer, this._world, this._clock, zaggerStartingPoint, zaggerHomePosition);
+            this._world.addActor(zagger);
 
-                const worldDimensions = this._world.dimensions;
-                const zaggerStartingPoint = new Point(
-                    Math.floor(random(0, worldDimensions.width-50)),
-                    -20
-                );
-                const zaggerHomeX = (5-(this._numberOfEnemiesLeftToDeploy-1)%5) * 80;
-                const zaggerHomeY = 100 + Math.floor((this._numberOfEnemiesLeftToDeploy-1)/5) * 70;
-                const zaggerHomePosition = new Point(zaggerHomeX, zaggerHomeY);
-                const zagger = new Zagger(this._audioPlayer, this._world, this._clock, zaggerStartingPoint, zaggerHomePosition);
-                this._world.addActor(zagger);
+            this._numberOfEnemiesLeftToDeploy--;
 
-                this._numberOfEnemiesLeftToDeploy--;
-            }
+            this._scheduler.scheduleOperation(
+                'deploy',
+                250,
+                () => { this.deployZagger() }
+            );
         }
     }
 }
